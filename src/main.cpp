1ed4c9a557dd910906b5d88d17cb84e1237cea83
Gudov <dima@novator.su>
2018-07-05 14:40:03 +0700

    add drawable object and improve map class

--- src/main.cpp
+++ src/main.cpp
@@ -9,22 +9,53 @@
 
 class MainApp : public re::IBaseApp{
 public:
+    re::Game world;
     re::Camera camera;
     Map map;
+    re::Point2f cam_pos;
+    float zoom;
 
     void setup() override {
-        map = Map( "map.tmx" );
+        map = Map( world, "map.tmx" );
         camera.view_at( re::Point2f(0,0) );
-        camera.scale( 1 );
+        camera.scale( 10 );
     }
 
     void update() override {
     }
 
     void display() override {
+        map.draw(camera);
     }
 
     void on_key_pressed(re::Key key){
+        switch( key )
+        {
+        case re::Key::W:
+            cam_pos -= re::Point2f( 0,20 );
+            camera.view_at( cam_pos );
+            break;
+        case re::Key::S:
+            cam_pos -= re::Point2f( 0,-20 );
+            camera.view_at( cam_pos );
+            break;
+        case re::Key::A:
+            cam_pos -= re::Point2f( 20,0 );
+            camera.view_at( cam_pos );
+            break;
+        case re::Key::D:
+            cam_pos -= re::Point2f( -20,0 );
+            camera.view_at( cam_pos );
+            break;
+        case re::Key::Q:
+            zoom += 0.5;
+            camera.scale( zoom );
+            break;
+        case re::Key::E:
+            zoom -= 0.5;
+            camera.scale( zoom );
+            break;
+        }
     }
 };
 
--- src/map.h
+++ src/map.h
@@ -1,14 +1,18 @@
 #pragma once
 
 #include <RealEngine/tiled_parser.h>
+#include <RealEngine/camera.h>
+
+#include "drawable_game_object.h"
 
 class Map
 {
 public:
     Map();
-    Map( std::string path );
-    void draw();
+    Map( re::Game &world,std::string path );
+    void draw( re::Camera camera );
 
+    std::vector<std::shared_ptr<DrawableGameObject>> objects;
 private:
     re::Map map;
 };
