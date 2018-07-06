d6c6fedf82ccd394316a560157670bddd36e2703
matvey <matrop@mail.ru>
2018-07-06 21:10:09 +0700

    new func. to check physic map

--- src/gamelogic/gamelogic.h
+++ src/gamelogic/gamelogic.h
@@ -2,10 +2,12 @@
 #include <RealEngine/physic_core.h>
 #include <RealEngine/camera.h>
 #include "../map.h"
+#include "../obstacles_generator.h"
 
 class GameLogic : public re::EventSubscriber
 {
 public:
+    std::vector<std::vector<int>> obstacles;
     GameLogic();
     virtual void on_event(std::shared_ptr<re::Event> event);
     void update();
