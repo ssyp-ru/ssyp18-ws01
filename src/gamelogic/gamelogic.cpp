ba5b67c94d58e60390508589593e9d3aca15f62a
matvey <matrop@mail.ru>
2018-07-07 15:55:56 +0700

    map generation in TXT file "map.txt"

--- src/gamelogic/gamelogic.cpp
+++ src/gamelogic/gamelogic.cpp
@@ -2,11 +2,14 @@
 #include "gamelogic.h"
 #include "../events/event_enum.h"
 #include <RealEngine/physic_core.h>
+#include <fstream>
+
+using namespace std;
 
 GameLogic::GameLogic()
 {
     this->map = Map( world, "map.tmx" );
-    // obstacles = Generate_obstacles(50, 100, 100, world);
+    obstacles = generate_obstacles(20, 250, 250, world);
 }
 
 void GameLogic::on_event(std::shared_ptr<re::Event> event)
@@ -21,17 +24,25 @@ void GameLogic::update()
 
 void GameLogic::draw( re::Camera camera )
 {
+    ofstream fout;
+    fout.open("map.txt");
     map.draw(camera);
     for( auto object : this->world.getWorld() )
     {
         auto drawable_object = std::static_pointer_cast<DrawableGameObject,re::PhysicObject>( object );
         drawable_object->display( camera );
     }
-    for (int i = 0; i < 100; i++){
-        for (int j = 0; j < 100; j++){
+    for (int i = 0; i < 250; i++){
+        for (int j = 0; j < 250; j++){
             if (obstacles[i][j] == 1){
-                re::draw_rectangle(j * 5, i * 5, 5, 5, re::Color(0, 0, 0));
+                fout << obstacles[i][j];
+                re::draw_rectangle(j * 2, i * 2, 2, 2, re::Color(0, 0, 0));
+            }
+            else{
+                fout << obstacles[i][j];
             }
         }
+        fout << std::endl;
     }
+    fout.close();
 }
