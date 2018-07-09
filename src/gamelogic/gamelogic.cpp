7e36be5c09f48089c5cf98565f699441e1b01c57
Gudov <dima@novator.su>
2018-07-09 18:56:02 +0700

    fix attack multithreading

--- src/gamelogic/gamelogic.cpp
+++ src/gamelogic/gamelogic.cpp
@@ -82,7 +82,9 @@ void GameLogic::on_event(std::shared_ptr<re::Event> event) {
                 {
                     auto attack_event = std::dynamic_pointer_cast<AttackEvent,re::Event>( event );
                     Unit* player = (Unit*)GameObject::get_object_by_id( attack_event->player_id );
-                    player->attack( attack_event->target_id );
+                    if( Unit != nullptr ) {
+                        player->attack( attack_event->target_id );
+                    }
                     return;
                 }
             }
